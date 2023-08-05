d0400b70a55ef0efc1fe5e0cbd7091d91ce1054e
Aryaman <98378052+Salty-Bear@users.noreply.github.com>
2023-08-05 23:48:45 +0530

    Update Otratwo.py
    Successfully added title scraping section

--- Otratwo.py
+++ Otratwo.py
@@ -14,23 +14,40 @@ options=webdriver.ChromeOptions()
 service=Service(executable_path=chrome_driver_path)
 driver=webdriver.Chrome(service=service)#,options=options)
 driver.get(url)
-but=driver.find_elements(By.XPATH, '//*[@id="app"]/div[3]/div/div/div[4]/div[2]/div[2]/div[2]')
-but[0].click()
+but=driver.find_element(By.XPATH, '//*[@id="app"]/div[3]/div/div/div[4]/div[2]/div[2]/div[2]')
+but.click()
 # today=driver.find_elements(By.ID, "active-day")
 # lol=driver.find_elements(By.CSS_SELECTOR, "a .show-title-bar")
 anime=driver.find_elements(By.CLASS_NAME, "description")
-name=driver.find_elements(By.CLASS_NAME, "title")
+
+name=driver.find_elements(By.CSS_SELECTOR, "div.date")
+links=driver.find_elements(By.CSS_SELECTOR,"img.image")
+
+title=driver.find_elements(By.CLASS_NAME, "title")
+
 # time=today[0].find_elements(By.CSS_SELECTOR, "h3 .show-air-time")
 ts=[]
 all=[]
+lin=[]
+titl=[]
 
 for ded in anime:
 	all.append(str(ded.text))
+
 for x in name:
 	ts.append(str(x.text))
+
+for x in links:
+	lin.append(str(x.get_attribute('src')))
+
+for x in title:
+	titl.append(str(x.text))
+
 # for x in range(len(time)):
 #     if anime[x].text != "":
 #         ts[anime[x].text]=time[x].text
 print(all)
-print(x)
+print(ts)
+print(lin)
+print(titl)
 driver.quit()
